7a3ab7f6d8669e932a919bee37790f3ec1aaade8
santa-333 <vigneshsanta2002@gmail.com>
2023-11-08 11:18:28 +0530

    santa projects

---
+++ cart.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+</head>
+<body>
+    <div class="main">
+    <div class="card" style="width: 18rem;">
+    <img src="https://m.media-amazon.com/images/I/61V5HLOJhTL._AC_UL600_FMwebp_QL65_.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+    <h5 class="card-title">WATCH</h5>
+    <button onclick="santa()" id="b1" class="btn btn-primary">ADD to Cart</button>
+  </div>
+  <div class="card" style="width: 18rem;">
+    <img src="https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/24508570/2023/8/16/fd802011-4755-4aeb-819f-538aaebfda1416921528423064-packbracelets1.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+    <h5 class="card-title">BRACELET</h5>
+    <button onclick="santa()" id="b2" class="btn btn-primary">ADD to Cart</button>
+  </div>
+  <div class="card" style="width: 18rem;">
+    <img src="https://images.cltstatic.com/media/product/350/AL00565-SS0000-infinite-joy-necklace-in-rhodium-plated--silver-prd-1-base.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+    <h5 class="card-title">CHAIN</h5>
+    <button onclick="santa()" id="b3" class="btn btn-primary">ADD to Cart</button>
+  </div>
+</div>
+
+
+
+
+    </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+</body>
+</html>
